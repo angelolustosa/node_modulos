68b1cb46c6a65ea5813dd625dc073cf99979c70f
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-10-28 20:29:34 -0300

    Adicionando o chalk para cor no terminal, modulo externo

--- modulo.js
+++ modulo.js
@@ -1,12 +1,25 @@
 const os = require('os');
 /* importa o modulo customizado */
-const { converterByteToMb } = require('./util/conversor.js')
+const { converterByteToMb } = require('./util/conversor.js');
+const chalk = require('chalk');
+
+
 const tipo = os.type();
 const plataforma = os.platform();
 const memoria_livre = os.freemem() //em bytes
+
+console.log(chalk.blue.bold(`=== Informação do Sistema ===\n`));
+
 
 console.log(`Tipo: ${plataforma}`);
 console.log(`Tipo: ${tipo}`);
 console.log(`Tipo: ${os.release()}`);
 console.log(`Tipo: ${memoria_livre} bytes`);
 console.log(`Tipo: ${converterByteToMb(memoria_livre)} MB`);
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
